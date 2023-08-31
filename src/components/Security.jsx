70dc7ab601122b9cefd4a0dd1b322bededb4a317
MiltonBlack <arziblack2@gmail.com>
2023-08-31 17:13:19 +0100

    security settings page api request and logic successful...

--- src/components/Security.jsx
+++ src/components/Security.jsx
@@ -5,8 +5,11 @@ import axios from 'axios'
 const Security = () => {
   const { user } = useSelector((state) => state.auth)
   const [details, setDetails] = useState({
-    password: user.password
+    password: user.password,
+    newPassword: "",
+    confirmPassword: ""
   });
+  const { password, newPassword, confirmPassword } = details;
   function onChange(e) {
     setDetails((prevState) => ({
       ...prevState,
@@ -19,32 +22,55 @@ const Security = () => {
         Authorization: `Bearer ${user.accessToken}`,
       },
     };
-    await axios
-      .put(
-        `http://localhost:3005/api/auth/settings/personnal/${user._id}`,
-        details,
-        config
-      )
-      .then((res) => console.log(res.data))
-      .catch((err) => console.log(err));
+    const data = {
+      newPassword
+    }
+    if (password === newPassword) {
+      console.log('New Password Cannot be Old Password');
+    } else if (newPassword !== confirmPassword) {
+      console.log('Passwords do not match');
+    } else {
+      await axios
+        .put(
+          `http://localhost:3005/api/auth/settings/personnal/${user._id}`,
+          data,
+          config
+        )
+        .then((res) => console.log(res.data))
+        .catch((err) => console.log(err));
+    }
+    console.log(data);
   }
+  console.log(details);
   return (
     <div className='p-4 w-[40%]'>
       <div className='my-4'>
         <h1>Old Password</h1>
         <input
           type="text"
-          className='border rounded my-1 w-full border-black'
+          className='border px-2 rounded my-1 w-full border-black'
           name='password'
-          onChange={() => { onChange() }} />
+          value={password}
+          onChange={onChange} />
       </div>
       <div className='my-4'>
         <h1>New Password</h1>
-        <input type="text" className='border rounded my-1 w-full border-black' />
+        <input
+          type="text"
+          className='border px-2 rounded my-1 w-full border-black'
+          name='newPassword'
+          value={newPassword}
+          onChange={onChange} />
       </div>
       <div className='my-4'>
         <h1>Confirm New Password</h1>
-        <input type="text" className='border rounded my-1 w-full border-black' />
+        <input
+          type="text"
+          className='border px-2 rounded my-1 w-full border-black'
+          name='confirmPassword'
+          value={confirmPassword}
+          onChange={onChange}
+        />
       </div>
       <button className='border p-2 w-full bg-black/75 text-white rounded' onClick={updatePassword}>Save</button>
     </div>
