5a5b7f109ecd4b0c19f60e9e12dac86094b79f45
MiltonBlack <arziblack2@gmail.com>
2023-09-14 15:28:40 +0100

    fix: replaced dummy Data on the admin dashboard home...

--- src/pages/admin/AdminDashHome.jsx
+++ src/pages/admin/AdminDashHome.jsx
@@ -1,11 +1,32 @@
 import React, { useEffect, useRef } from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { FaPlus, FaUser } from 'react-icons/fa'
 import Card from '../../components/Card'
+import { getAllDepositsAdmin, getAllUsersAdmin, getAllWithdrawalsAdmin } from '../../services/adminSlice';
 
 let tvScriptLoadingPromise;
 const AdminDashHome = () => {
-  const { admin } = useSelector((state) => state.admin);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllUsersAdmin());
+    dispatch(getAllDepositsAdmin());
+    dispatch(getAllWithdrawalsAdmin());
+  }, [dispatch]);
+  const { admin, allUsers, deposits, withdrawals } = useSelector((state) => state.admin);
+  const totalUsers = allUsers.length;
+  const pendingDeposits = deposits.find((item) => item.status === "pending");
+  const totalPendingDeposits = pendingDeposits.length;
+  const pendingDepositAmount = pendingDeposits.reduce((currentTotal, item) => {
+    return item.amount + currentTotal;
+  }, 0);
+  const totalDeposits = deposits.reduce((currentTotal, item) => {
+    return item.amount + currentTotal;
+  }, 0);
+  const noOfDeposits = deposits.length;
+  const pendingWithdrawals = withdrawals.reduce((currentTotal, item) => {
+    return item.withdrawAmount + currentTotal;
+  }, 0);
+  const totalPendingWithdawals = withdrawals.length;
   const onLoadScriptRef = useRef();
 
   useEffect(
@@ -51,97 +72,106 @@ const AdminDashHome = () => {
   );
   return (
     <>
-    <div className=' bg-stone-200 w-full h-full flex flex-col px-5 md:px-10 pt-16'>
-      <div className='flex w-full justify-between items-center my-8'>
-        <div>
-          <h1 className=' font-black text-2xl'>Hello, Admin! {admin.fullName}</h1>
-          <span className='font-light text-sm text-slate-600'>We recommend you to select a subscription package for an investment plan today</span>
+      <div className=' bg-stone-200 w-full h-full flex flex-col px-5 md:px-10 pt-16'>
+        <div className='flex w-full justify-between items-center my-8'>
+          <div>
+            <h1 className=' font-black text-2xl'>Hello, Admin! {admin.fullName}</h1>
+            <span className='font-light text-sm text-slate-600'>Take full control of your Users, their Deposits and Withdrawals</span>
+          </div>
+          <div className='border border-black p-1 bg-rose-500'>
+            <FaPlus color='white' />
+          </div>
         </div>
-        <div className='border border-black p-1 bg-rose-500'>
-          <FaPlus color='white' />
-        </div>
-      </div>
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 sm:gap-[5%]'>
-        <Card>
-          <div className='flex flex-col h-full w-full justify-between'>
-            <div className='flex w-full justify-between items-center'>
-              <FaUser size={30} />
-              <span>All Users:</span>
-              <span>26</span>
-            </div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 sm:gap-[5%]'>
+          <Card>
+            <div className='flex flex-col h-full w-full justify-between'>
+              <div className='flex w-full justify-between items-center'>
+                <FaUser size={30} />
+                <span>All Users:</span>
+                <span>{totalUsers}</span>
+              </div>
               <span className='font-light text-sm text-slate-600 mt-4'>25% This week</span>
+            </div>
+          </Card>
+          <Card>
+            <div className='flex flex-col h-full w-full justify-between'>
+              <div className='flex w-full justify-between items-center'>
+                <span>Pending Deposits:</span>
+                <span>${pendingDepositAmount}</span>
+              </div>
+              <div className='flex w-full justify-between items-center'>
+                <span className='font-light text-sm text-slate-600 mt-4'>No:</span>
+                <span className='font-light text-sm text-slate-600 mt-4'>{totalPendingDeposits}</span>
+              </div>
+            </div>
+          </Card>
+          <Card>
+            <div className='flex flex-col h-full w-full justify-between'>
+              <div className='flex w-full justify-between items-center'>
+                <span>Total Deposits:</span>
+                <span>${totalDeposits}</span>
+              </div>
+              <div className='flex w-full justify-between items-center'>
+                <span className='font-light text-sm text-slate-600 mt-4'>No:</span>
+                <span className='font-light text-sm text-slate-600 mt-4'>{noOfDeposits}</span>
+              </div>
+            </div>
+          </Card>
+          <Card>
+            <div className='flex flex-col h-full w-full justify-between'>
+              <div className='flex w-full justify-between items-center'>
+                <span>Pending Withdrawals:</span>
+                <span>${pendingWithdrawals}</span>
+              </div>
+              <div className='flex w-full justify-between items-center'>
+                <span className='font-light text-sm text-slate-600 mt-4'>No:</span>
+                <span className='font-light text-sm text-slate-600 mt-4'>{totalPendingWithdawals}</span>
+              </div>
+            </div>
+          </Card>
+        </div>
+        <div className='flex w-full justify-between items-center my-8'>
+          <div>
+            <h1 className=' font-black text-2xl'>Market Overview</h1>
+            <span className='font-light text-sm text-slate-600'>Prices, Volume Updates</span>
+          </div>
+          <div className='border border-black text-sm font-normal rounded shadow-md'>
+            <select name="" id="" className='p-1 rounded-sm'>
+              <option value="a">Weekly (2023)</option>
+            </select>
+          </div>
+        </div>
+        <Card>
+          <div className='flex justify-center items-center w-full font-light'>
+            {/* ( " No Trading Data Available Please Subscribe to get Trading Data!") */}
+            <div className='tradingview-widget-container'>
+              <div id='tradingview_9bd40' />
+              <div className="tradingview-widget-copyright">
+                <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">Track all markets on TradingView</span></a>
+              </div>
+            </div>
           </div>
         </Card>
+        <div className='flex w-full justify-between items-center my-8'>
+          <div>
+            <h1 className=' font-black text-2xl'>Recent Activities</h1>
+            <span className='font-light text-sm text-slate-600'>Subscription Package</span>
+          </div>
+          <div className='border border-black text-sm font-normal rounded shadow-md'>
+            <select name="" id="" className='p-1 rounded-sm'>
+              <option value="a">Monthly (2023)</option>
+            </select>
+          </div>
+        </div>
         <Card>
-        <div className='flex flex-col h-full w-full justify-between'>
-            <div className='flex w-full justify-between items-center'>
-              <span>Pending Deposits:</span>
-              <span>$2,000</span>
-            </div>
-              <span className='font-light text-sm text-slate-600 mt-4'>25% This week</span>
+          <div className='flex justify-center items-center w-full font-light'>
+            "You are on the Premium Plan":"Not Subscribed to any Packages"
           </div>
         </Card>
-        <Card>
-        <div className='flex flex-col h-full w-full justify-between'>
-            <div className='flex w-full justify-between items-center'>
-              <span>Total Deposits:</span>
-              <span>$45,000</span>
-            </div>
-              <span className='font-light text-sm text-slate-600 mt-4'>25% This week</span>
-          </div>
-        </Card>
-        <Card>
-        <div className='flex flex-col h-full w-full justify-between'>
-            <div className='flex w-full justify-between items-center'>
-              <span>Pending Withdrawals:</span>
-              <span>$149,000</span>
-            </div>
-              <span className='font-light text-sm text-slate-600 mt-4'>25% This week</span>
-          </div>
-        </Card>
+        <div className='py-8'></div>
       </div>
-      <div className='flex w-full justify-between items-center my-8'>
-        <div>
-          <h1 className=' font-black text-2xl'>Market Overview</h1>
-          <span className='font-light text-sm text-slate-600'>Prices, Volume Updates</span>
-        </div>
-        <div className='border border-black text-sm font-normal rounded shadow-md'>
-          <select name="" id="" className='p-1 rounded-sm'>
-            <option value="a">Weekly (2023)</option>
-          </select>
-        </div>
-      </div>
-      <Card>
-        <div className='flex justify-center items-center w-full font-light'>
-          {/* ( " No Trading Data Available Please Subscribe to get Trading Data!") */}
-          <div className='tradingview-widget-container'>
-            <div id='tradingview_9bd40' />
-            <div className="tradingview-widget-copyright">
-              <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span className="blue-text">Track all markets on TradingView</span></a>
-            </div>
-          </div>
-        </div>
-      </Card>
-      <div className='flex w-full justify-between items-center my-8'>
-        <div>
-          <h1 className=' font-black text-2xl'>Recent Activities</h1>
-          <span className='font-light text-sm text-slate-600'>Subscription Package</span>
-        </div>
-        <div className='border border-black text-sm font-normal rounded shadow-md'>
-          <select name="" id="" className='p-1 rounded-sm'>
-            <option value="a">Monthly (2023)</option>
-          </select>
-        </div>
-      </div>
-      <Card>
-        <div className='flex justify-center items-center w-full font-light'>
-           "You are on the Premium Plan":"Not Subscribed to any Packages"
-        </div>
-      </Card>
-      <div className='py-8'></div>
-    </div>
-  </>
-  ) 
+    </>
+  )
 }
 
 export default AdminDashHome
