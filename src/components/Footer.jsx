cbb48063edbbc2ea9ff5b688d754d60d801830e0
MiltonBlack <arziblack2@gmail.com>
2023-09-17 11:57:46 +0100

    fix: change BTCPAY content in Folder...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className='grid grid-cols-3 md:grid-cols-5 gap-5'>
       <div>
         <h1 className='font-normal text-md my-2'>Bitcoin Exchange Trading and Investment Platform</h1>
-        <p>cd s vosijcojs svsopjcvo s opsjcos vsov svsojvojo</p>
+        <p>Explore its Potential for Investments and its Impacts on the Global Economy. Whether you're a curious beginner or looking to deepen your understanding, This is your Gateway to all things Bitcoin.</p>
       </div>
       <div>
         <h1 className='font-normal text-md my-2'>Trading</h1>
